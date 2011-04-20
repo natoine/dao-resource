75930770e2e305e1cab808ed7b623b1e827e3598
Natoine <antoineseilles@gmail.com>
2011-04-20 13:19:50 +0200

    lgpl correction

--- src/main/java/fr/natoine/dao/resource/DAOResource.java
+++ src/main/java/fr/natoine/dao/resource/DAOResource.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2010 Antoine Seilles (Natoine)
- *   This file is part of controler-resource.
+ *   This file is part of dao-resource.
 
     controler-resource is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
