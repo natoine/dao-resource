31c425766c9edae7c6d14f4d754e592c7ff20b38
Natoine <antoineseilles@gmail.com>
2011-04-20 13:23:53 +0200

    lgpl correction

--- src/main/java/fr/natoine/dao/resource/DAOResource.java
+++ src/main/java/fr/natoine/dao/resource/DAOResource.java
@@ -13,7 +13,7 @@
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with controler-resource.  If not, see <http://www.gnu.org/licenses/>.
+    along with dao-resource.  If not, see <http://www.gnu.org/licenses/>.
 
  */
 
